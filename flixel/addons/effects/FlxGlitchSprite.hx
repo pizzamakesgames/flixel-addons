1eac102c60a8ce293f53428778c58b1482b923b0
Zaphod <zaphy@yandex.ru>
2015-09-11 19:18:26 +0300

    updated glitch and wave sprites

--- flixel/addons/effects/FlxGlitchSprite.hx
+++ flixel/addons/effects/FlxGlitchSprite.hx
@@ -1,11 +1,15 @@
 package flixel.addons.effects;
 
+import flixel.addons.effects.FlxGlitchSprite.FlxGlitchView;
+import flixel.FlxBaseSprite;
 import flixel.FlxSprite;
 import flixel.graphics.FlxTexture;
 import flixel.graphics.views.FlxGraphic;
+import flixel.graphics.views.FlxImage;
 import flixel.util.FlxColor;
 import flixel.util.FlxDestroyUtil;
 import flixel.math.FlxRandom;
+import openfl.display.BitmapData;
 
 /**
  * This creates a FlxSprite which copies a target FlxSprite and applies a non-destructive wave-distortion effect.
@@ -16,28 +20,27 @@ import flixel.math.FlxRandom;
  */
 class FlxGlitchSprite extends FlxSprite
 {
+	public var glitchView(default, null):FlxGlitchView;
 	/**
 	 * How thick each glitch segment should be.
 	 */
-	public var size:Int = 1;
+	public var size(get, set):Int;
 	/**
 	 * Time, in seconds, between glitch updates
 	 */
-	public var delay:Float = 0.05;
+	public var delay(get, set):Float;
 	/**
 	 * The target FlxSprite that the glitch effect copies from.
 	 */
-	public var target:FlxSprite;
+	public var target(get, set):FlxBaseSprite;
 	/**
 	 * Which direction the glitch effect should be applied.
 	 */
-	public var direction(default, set):FlxGlitchDirection;
+	public var direction(get, set):FlxGlitchDirection;
 	/**
 	 * How strong the glitch effect should be (how much it should move from the center)
 	 */
-	public var strength(default, set):Int = 2;
-	
-	private var _time:Float = 0;
+	public var strength(get, set):Int;
 	
 	/**
 	 * Creates a new FlxGlitchSprite, which clones a target FlxSprite and applies a Glitch-distortion effect to the clone.
@@ -52,11 +55,126 @@ class FlxGlitchSprite extends FlxSprite
 	public function new(Target:FlxSprite, Strength:Int = 4, Size:Int = 1, Delay:Float = 0.05, ?Direction:FlxGlitchDirection) 
 	{
 		super();
+		graphic = glitchView = new FlxGlitchView(this, Target, Strength, Size, Delay, Direction);
+	}
+	
+	override public function destroy():Void 
+	{
+		super.destroy();
+		glitchView = null;
+	}
+	
+	private function get_size():Int
+	{
+		return glitchView.size;
+	}
+	
+	private function set_size(Value:Int):Int
+	{
+		return glitchView.size = Value;
+	}
+	
+	private function get_delay():Float
+	{
+		return glitchView.delay;
+	}
+	
+	private function set_delay(Value:Float):Float
+	{
+		return glitchView.delay = Value;
+	}
+	
+	private function get_target():FlxBaseSprite
+	{
+		return glitchView.target;
+	}
+	
+	private function set_target(Value:FlxBaseSprite):FlxBaseSprite
+	{
+		return glitchView.target = Value;
+	}
+	
+	private function get_direction():FlxGlitchDirection
+	{
+		return glitchView.direction;
+	}
+	
+	private function set_direction(Value:FlxGlitchDirection):FlxGlitchDirection
+	{
+		return glitchView.direction = Value;
+	}
+	
+	private function get_strength():Int
+	{
+		return glitchView.strength;
+	}
+	
+	private function set_strength(Value:Int):Int
+	{
+		return glitchView.strength = Value;
+	}
+}
+
+enum FlxGlitchDirection
+{
+	HORIZONTAL;
+	VERTICAL;
+}
+
+class FlxGlitchView extends FlxImage
+{
+	/**
+	 * How thick each glitch segment should be.
+	 */
+	public var size(default, set):Int = 1;
+	/**
+	 * Time, in seconds, between glitch updates
+	 */
+	public var delay:Float = 0.05;
+	/**
+	 * The target FlxSprite that the glitch effect copies from.
+	 */
+	public var target(default, set):FlxBaseSprite;
+	/**
+	 * Which direction the glitch effect should be applied.
+	 */
+	public var direction(default, set):FlxGlitchDirection;
+	/**
+	 * How strong the glitch effect should be (how much it should move from the center)
+	 */
+	public var strength(default, set):Int = 2;
+	
+	private var _time:Float = 0;
+	
+	private var _regen:Bool = true;
+	private var _update:Bool = true;
+	
+	/**
+	 * Creates a new FlxGlitchSprite, which clones a target FlxSprite and applies a Glitch-distortion effect to the clone.
+	 * This effect is non-destructive to the target's pixels, and can be used on animated FlxSprites.
+	 * 
+	 * @param	Target		The target FlxSprite you want to clone.
+	 * @param	Strength	How strong you want the effect
+	 * @param	Size		How 'thick' you want each piece of the glitch
+	 * @param	Delay		How long (in seconds) between each glitch update
+	 * @param	Direction	Which Direction you want the effect to be applied (HORIZONTAL or VERTICAL)
+	 */
+	public function new(Parent:FlxBaseSprite, Target:FlxBaseSprite, Strength:Int = 4, Size:Int = 1, Delay:Float = 0.05, ?Direction:FlxGlitchDirection) 
+	{
+		super(Parent);
 		target = Target;
 		strength = Strength;
 		size = Size;
 		direction = (Direction != null) ? Direction : HORIZONTAL;
 		initPixels();
+	}
+	
+	override public function destroy():Void 
+	{
+		super.destroy();
+		
+		target = null;
+		direction = null;
 	}
 	
 	override public function update(elapsed:Float):Void
@@ -77,55 +195,93 @@ class FlxGlitchSprite extends FlxSprite
 	{
 		if (alpha == 0 || target == null)
 			return;
-			
-		if (_time == 0)
-		{
-			_time = 0;
-			pixels.lock();
-			pixels.fillRect(pixels.rect, FlxColor.TRANSPARENT);
-			var p:Int = 0;
-			if (direction == HORIZONTAL)
-			{
-				while (p < target.frameHeight) 
-				{
-					_flashRect2.setTo(0, p, target.frameWidth, size);
-					if (_flashRect2.bottom > target.frameHeight)
-						_flashRect2.bottom = target.frameHeight;
-					_flashPoint.setTo(FlxG.random.int( -strength, strength) + strength, p);
-					p += Std.int(_flashRect2.height);
-					pixels.copyPixels(target.framePixels, _flashRect2, _flashPoint);
-				}
-			}
-			else
-			{
-				while (p < target.frameWidth) 
-				{
-					_flashRect2.setTo(p, 0, size, target.frameHeight);
-					if (_flashRect2.right > target.frameWidth)
-						_flashRect2.right = target.frameWidth;
-					_flashPoint.setTo(p, FlxG.random.int( -strength, strength) + strength);
-					p += Std.int(_flashRect2.width);
-					pixels.copyPixels(target.framePixels, _flashRect2, _flashPoint);
-				}
-			}
-			
-			pixels.unlock();
-			dirty = true;
-		}
+		
+		if (_regen)
+			initPixels();
+		
+		if (_time == 0 || _update)
+			updatePixels();
 		
 		super.draw();
 	}
 	
+	private function updatePixels():Void
+	{
+		_time = 0;
+		pixels.lock();
+		_flashRect2.setTo(0, 0, texture.width, texture.height);
+		pixels.fillRect(_flashRect2, FlxColor.TRANSPARENT);
+		var targetPixels:BitmapData = target.graphic.getFlxFrameBitmapData();
+		var p:Int = 0;
+		
+		if (direction == HORIZONTAL)
+		{
+			while (p < target.frameHeight) 
+			{
+				_flashRect2.setTo(0, p, target.frameWidth, size);
+				if (_flashRect2.bottom > target.frameHeight)
+					_flashRect2.bottom = target.frameHeight;
+				
+				_flashPoint.setTo(FlxG.random.int( -strength, strength) + strength, p);
+				p += Std.int(_flashRect2.height);
+				pixels.copyPixels(targetPixels, _flashRect2, _flashPoint);
+			}
+		}
+		else
+		{
+			while (p < target.frameWidth) 
+			{
+				_flashRect2.setTo(p, 0, size, target.frameHeight);
+				if (_flashRect2.right > target.frameWidth)
+					_flashRect2.right = target.frameWidth;
+				
+				_flashPoint.setTo(p, FlxG.random.int( -strength, strength) + strength);
+				p += Std.int(_flashRect2.width);
+				pixels.copyPixels(targetPixels, _flashRect2, _flashPoint);
+			}
+		}
+		
+		pixels.unlock();
+		dirty = true;
+	}
+	
 	private function initPixels():Void
 	{
-		var oldGraphic:FlxGraphic = graphic;
-		target.drawFrame(true);
-		setPosition(target.x - (direction == HORIZONTAL ? strength : 0), target.y - (direction == VERTICAL ? strength : 0));
-		makeGraphic(Std.int(target.frameWidth + (direction == HORIZONTAL ? strength * 2 : 0)), Std.int(target.frameHeight + (direction == VERTICAL ? strength * 2 : 0 )), FlxColor.TRANSPARENT, true);
+		if (!_regen)	return;
+		
+		var oldTexture:FlxTexture = texture;
+		var oldWidth:Int = 0;
+		var oldHeight:Int = 0;
+		
+		if (oldTexture != null)
+		{
+			oldWidth = oldTexture.width;
+			oldHeight = oldTexture.height;
+		}
+		
+		parent.setPosition(target.x - (direction == HORIZONTAL ? strength : 0), target.y - (direction == VERTICAL ? strength : 0));
+		
+		var targetPixels:BitmapData = target.graphic.getFlxFrameBitmapData();
+		var newWidth:Int = target.frameWidth + (direction == HORIZONTAL ? strength * 2 : 0);
+		var newHeight:Int = target.frameHeight + (direction == VERTICAL ? strength * 2 : 0 );
+		
+		if (newWidth != oldWidth || newHeight != oldHeight)
+		{
+			makeGraphic(newWidth, newHeight, FlxColor.TRANSPARENT, true);
+		}
+		else
+		{
+			_flashRect2.setTo(0, 0, texture.width, texture.height);
+			texture.bitmap.fillRect(_flashRect2, FlxColor.TRANSPARENT);
+		}
+		
 		_flashPoint.setTo((direction == HORIZONTAL ? strength : 0), (direction == VERTICAL ? strength : 0));
-		pixels.copyPixels(target.framePixels, target.framePixels.rect, _flashPoint);
+		_flashRect2.setTo(0, 0, targetPixels.width, targetPixels.height);
+		pixels.copyPixels(targetPixels, _flashRect2, _flashPoint);
+		_flashRect2.setTo(0, 0, texture.width, texture.height);
 		dirty = true;
-		FlxG.bitmap.removeIfNoUse(oldGraphic);
+		_regen = false;
+		FlxG.bitmap.removeIfNoUse(oldTexture);
 	}
 	
 	private function set_direction(Value:FlxGlitchDirection):FlxGlitchDirection
@@ -133,7 +289,7 @@ class FlxGlitchSprite extends FlxSprite
 		if (direction != Value)
 		{
 			direction = Value;
-			initPixels();
+			_regen = true;
 		}
 		return direction;
 	}
@@ -143,14 +299,41 @@ class FlxGlitchSprite extends FlxSprite
 		if (strength != Value)
 		{
 			strength = Value;
-			initPixels();
+			_regen = true;
 		}
 		return strength;
 	}
-}
-
-enum FlxGlitchDirection
-{
-	HORIZONTAL;
-	VERTICAL;
+	
+	private function set_target(Value:FlxBaseSprite):FlxBaseSprite
+	{
+		if (target != Value)
+		{
+			target = Value;
+			_regen = true;
+		}
+		
+		return Value;
+	}
+	
+	private function set_size(Value:Int):Int
+	{
+		if (size != Value)
+		{
+			size = Value;
+			_update = true;
+		}
+		
+		return Value;
+	}
+	
+	override public function getFlxFrameBitmapData():BitmapData 
+	{
+		if (_regen)
+			initPixels();
+		
+		if (_time == 0 || _update)
+			updatePixels();
+		
+		return super.getFlxFrameBitmapData();
+	}
 }
--- flixel/addons/effects/FlxWaveSprite.hx
+++ flixel/addons/effects/FlxWaveSprite.hx
@@ -2,10 +2,13 @@ package flixel.addons.effects;
 
 import flash.geom.Point;
 import flash.geom.Rectangle;
+import flixel.addons.effects.FlxWaveSprite.FlxWaveView;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.graphics.FlxTexture;
+import flixel.graphics.views.FlxImage;
 import flixel.util.FlxColor;
+import openfl.display.BitmapData;
 
 /**
  * This creates a FlxSprite which copies a target FlxSprite and applies a non-destructive wave-distortion effect.
@@ -17,10 +20,103 @@ class FlxWaveSprite extends FlxSprite
 {
 	private static inline var BASE_STRENGTH:Float = 0.06;
 	
+	public var waveView(default, null):FlxWaveView;
+	
+	/**
+	 * Which mode we're using for the effect
+	 */
+	public var mode(get, set):FlxWaveMode;
+	/**
+	 * How fast should the wave effect be (higher = faster)
+	 */
+	public var speed(get, set):Float;
+	/**
+	 * The 'center' of our sprite (where the wave effect should start/end)
+	 */
+	public var center(get, set):Int;
+	/**
+	 * How strong the wave effect should be
+	 */
+	public var strength(get, set):Int;
+	
+	/**
+	 * Creates a new FlxWaveSprite, which clones a target FlxSprite and applies a wave-distortion effect to the clone.
+	 * 
+	 * @param	Target		The target FlxSprite you want to clone.
+	 * @param	Mode		Which Mode you would like to use for the effect. ALL = applies a constant distortion throughout the image, BOTTOM = makes the effect get stronger towards the bottom of the image, and TOP = the reverse of BOTTOM
+	 * @param	Strength	How strong you want the effect
+	 * @param	Center		The 'center' of the effect when using BOTTOM or TOP modes. Anything above(BOTTOM)/below(TOP) this point on the image will have no distortion effect.
+	 * @param	Speed		How fast you want the effect to move. Higher values = faster.
+	 */
+	public function new(Target:FlxSprite, ?Mode:FlxWaveMode, Strength:Int = 20, Center:Int = -1, Speed:Float = 3) 
+	{
+		super();
+		graphic = waveView = new FlxWaveView(this, Target, Mode, Strength, Center, Speed);
+	}
+	
+	override public function destroy():Void 
+	{
+		super.destroy();
+		waveView = null;
+	}
+	
+	private function get_strength():Int
+	{
+		return waveView.strength;
+	}
+	
+	private function set_strength(value:Int):Int 
+	{
+		return waveView.strength = value;
+	}
+	
+	private function get_mode():FlxWaveMode
+	{
+		return waveView.mode;
+	}
+	
+	private function set_mode(value:FlxWaveMode):FlxWaveMode
+	{
+		return waveView.mode = value;
+	}
+	
+	private function get_speed():Float
+	{
+		return waveView.speed;
+	}
+	
+	private function set_speed(value:Float):Float
+	{
+		return waveView.speed = value;
+	}
+	
+	private function get_center():Int
+	{
+		return waveView.center;
+	}
+	
+	private function set_center(value:Int):Int
+	{
+		return waveView.center = center;
+	}
+}
+
+enum FlxWaveMode
+{
+	ALL;
+	TOP;
+	BOTTOM;
+}
+
+class FlxWaveView extends FlxImage
+{
+	private static inline var BASE_STRENGTH:Float = 0.06;
+	
 	/**
 	 * Which mode we're using for the effect
 	 */
 	public var mode:FlxWaveMode;
+	
 	/**
 	 * How fast should the wave effect be (higher = faster)
 	 */
@@ -37,10 +133,12 @@ class FlxWaveSprite extends FlxSprite
 	/**
 	 * The target FlxSprite we're going to be using
 	 */
-	private var _target:FlxSprite;
+	private var _target:FlxBaseSprite;
 	private var _targetOffset:Float = -999;
 	
 	private var _time:Float = 0;
+	
+	private var _regen:Bool = true;
 	
 	/**
 	 * Creates a new FlxWaveSprite, which clones a target FlxSprite and applies a wave-distortion effect to the clone.
@@ -51,17 +149,25 @@ class FlxWaveSprite extends FlxSprite
 	 * @param	Center		The 'center' of the effect when using BOTTOM or TOP modes. Anything above(BOTTOM)/below(TOP) this point on the image will have no distortion effect.
 	 * @param	Speed		How fast you want the effect to move. Higher values = faster.
 	 */
-	public function new(Target:FlxSprite, ?Mode:FlxWaveMode, Strength:Int = 20, Center:Int = -1, Speed:Float = 3) 
+	public function new(Parent:FlxBaseSprite, Target:FlxSprite, ?Mode:FlxWaveMode, Strength:Int = 20, Center:Int = -1, Speed:Float = 3) 
 	{
-		super();
+		super(Parent);
 		_target = Target;
 		strength = Strength;
 		mode = (Mode == null) ? ALL : Mode;
 		speed = Speed;
 		if (Center < 0)
 			center = Std.int(_target.height * 0.5);
-		initPixels();
 		dirty = true;
+		initPixels();
+	}
+	
+	override public function destroy():Void 
+	{
+		super.destroy();
+		
+		_target = null;
+		mode = null;
 	}
 	
 	override public function update(elapsed:Float):Void
@@ -74,15 +180,21 @@ class FlxWaveSprite extends FlxSprite
 	{
 		if (!visible || alpha == 0)
 			return;
+			
+		if (_regen)
+			initPixels();
 		
 		pixels.lock();
-		pixels.fillRect(pixels.rect, FlxColor.TRANSPARENT);
+		_flashRect2.setTo(0, 0, texture.width, texture.height);
+		pixels.fillRect(_flashRect2, FlxColor.TRANSPARENT);
+		
+		var targetPixels:BitmapData = _target.graphic.getFlxFrameBitmapData();
 		
 		var offset:Float = 0;
 		for (oY in 0..._target.frameHeight)
 		{
-			var p:Float=0;
-			switch(mode)
+			var p:Float = 0;
+			switch (mode)
 			{
 				case ALL:
 					offset = center * calculateOffset(oY);
@@ -104,7 +216,7 @@ class FlxWaveSprite extends FlxSprite
 			
 			_flashPoint.setTo(strength + offset, oY);
 			_flashRect2.setTo(0, oY, _target.frameWidth, 1);
-			pixels.copyPixels(_target.framePixels, _flashRect2, _flashPoint);
+			pixels.copyPixels(targetPixels, _flashRect2, _flashPoint);
 		}
 		pixels.unlock();
 		
@@ -129,14 +241,41 @@ class FlxWaveSprite extends FlxSprite
 	
 	private function initPixels():Void
 	{
-		var oldGraphic:FlxGraphic = graphic;
-		_target.drawFrame(true);
-		setPosition(_target.x - strength, _target.y);
-		makeGraphic(Std.int(_target.frameWidth + (strength * 2)), _target.frameHeight, FlxColor.TRANSPARENT, true);
+		if (!_regen)	return;
+		
+		var oldTexture:FlxTexture = texture;
+		var oldWidth:Int = 0;
+		var oldHeight:Int = 0;
+		
+		if (oldTexture != null)
+		{
+			oldWidth = oldTexture.width;
+			oldHeight = oldTexture.height;
+		}
+		
+		parent.setPosition(_target.x - strength, _target.y);
+		
+		var targetPixels:BitmapData = _target.graphic.getFlxFrameBitmapData();
+		var newWidth:Int = _target.frameWidth + (strength * 2);
+		var newHeight:Int = _target.frameHeight;
+		
+		if (newWidth != oldWidth || newHeight != oldHeight)
+		{
+			makeGraphic(newWidth, newHeight, FlxColor.TRANSPARENT, true);
+		}
+		else
+		{
+			_flashRect2.setTo(0, 0, texture.width, texture.height);
+			texture.bitmap.fillRect(_flashRect2, FlxColor.TRANSPARENT);
+		}
+		
 		_flashPoint.setTo(strength, 0);
-		pixels.copyPixels(_target.framePixels, _target.framePixels.rect, _flashPoint);
+		_flashRect2.setTo(0, 0, targetPixels.width, targetPixels.height);
+		pixels.copyPixels(targetPixels, _flashRect2, _flashPoint);
+		_flashRect2.setTo(0, 0, texture.width, texture.height);
 		dirty = true;
-		FlxG.bitmap.removeIfNoUse(oldGraphic);
+		_regen = false;
+		FlxG.bitmap.removeIfNoUse(oldTexture);
 	}
 	
 	private function set_strength(value:Int):Int 
@@ -144,15 +283,9 @@ class FlxWaveSprite extends FlxSprite
 		if (strength != value)
 		{
 			strength = value;
-			initPixels();
+			_regen = true;
 		}
+		
 		return strength;
 	}
-}
-
-enum FlxWaveMode
-{
-	ALL;
-	TOP;
-	BOTTOM;
 }
